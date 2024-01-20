5f99b2029025635ad99ae12b235ab1f2cb5bfeea
Anna Moon <anna@fourmoonstech.com>
2024-01-19 22:23:35 -0600

    edited css file, handlebars

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,22 +1,16 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-  
-    
+    event.preventDefault(); 
     const username = document.querySelector('#username-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (first_name && last_name && email && password) {
+    if ( username && email && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({ username, email, password }),
   
         headers: { 'Content-Type': 'application/json' },
       });
-  
-      
-  
       if (response.ok) {
         document.location.replace('/dashboard');
         console.log('success!')
