ef6ed3a45378d865ce7eed3259b7888742c54857
Anna Moon <anna@fourmoonstech.com>
2024-01-27 22:23:10 -0600

    edited partial for post to modufy card, modified create post form

--- public/js/login.js
+++ public/js/login.js
@@ -11,7 +11,7 @@ const loginFormHandler = async (event) => {
         headers: { 'Content-Type': 'application/json' },
       });
   
-      console.log(response)
+      // console.log(response)
   
       if (response.ok) {
         //console.log(`Successful login as ${username}`)
